622dac230c7785db33dfa01e6785607668dbf917
Thanas R <thanas5.rd@gmail.com>
2025-12-28 19:38:35 +0000

    Visual edit in
    Edited UI in

--- src/components/NavigationMenu.tsx
+++ src/components/NavigationMenu.tsx
@@ -100,7 +100,7 @@ export const NavigationMenu = ({
                   Server Contents
                 </motion.button>
 
-                <div className="flex flex-wrap gap-3 pt-4 sm:pt-6">
+                <div className="flex-wrap gap-3 pt-4 sm:pt-6 flex items-start justify-start">
                   <motion.a initial={{
                 opacity: 0,
                 y: 10
